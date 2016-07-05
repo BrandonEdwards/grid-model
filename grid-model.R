81e09098ecab605d0f61fb9516a77f0fc7800767
Brandon Edwards <brandon.edwards144@gmail.com>
2016-07-05 15:22:40 -0400

    Added functionality to grab data based on 5 steps of the grid

--- grid-model.R
+++ grid-model.R
@@ -29,6 +29,19 @@ AnalysisYear<-2015
 #	Load Data
 ####################################	
 
-HER<-read.csv(paste("C:/Users/Brandon/SkyDrive/URA/grid-model/", AnalysisYear, "_Harvest_Data_All.csv", sep=""))
+HER<-read.csv(paste("C:/Users/Brandon/Documents/GitHub/grid-model/", AnalysisYear, "_Harvest_Data_All.csv", sep=""))
 
-#HER.MERGE<-HER[HER$ZONE=="4-1", "4-2", "4-3", "4-7", "4-4", "4-5", ]
+HER.MERGE<-HER[ ! HER$ZONE %in% c("6-1", "6-3", "5-1", "5-2", "5-3", "5-4", "5-5", "5-6", "5-7", "5-8", "5-9"), ]
+HER.MERGE<-HER.MERGE[order(-HER.MERGE$GRID),]
+
+####################################
+# Analysis of Stepped Grids
+####################################
+
+#Get unique grid numbers
+grid <- unique(HER.MERGE$GRID)
+
+for (i in 5:length(grid))
+{
+  HER.ANALYZE<-HER.MERGE[HER.MERGE$GRID %in% c(grid[i], grid[i-1], grid[i-2], grid[i-3], grid[i-4]), ]
+}
